cd586f38e9658b31d56da8adfba321feebe1aa8b
janith3 <janith@edhirya.com>
2022-05-10 23:58:28 +0530

    initial commit

---
+++ src/main/java/com/usermanagementservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.usermanagementservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-service")
+public class UserController {
+
+    @GetMapping("/user")
+    public ResponseEntity<String> extractOrdersFromLogicBroker() {
+        return ResponseEntity.ok("hello world");
+    }
+}
